f73a34cceb90bb057736d3757f445ed80e9b5e38
waynetheisinger <wayne@mowdirect.co.uk>
2023-09-29 07:24:18 +0100

    docs: comment for instruction

--- app.js
+++ app.js
@@ -1,51 +1,94 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const { Worker } = require('worker_threads');
-const Queue = require('bull');
-const os = require('os');
+/**
+ * Concept of Worker Threads:
+ * --------------------------
+ * Node.js runs JavaScript on the V8 engine in a single-threaded event loop, 
+ * which can be a performance bottleneck when handling CPU-bound tasks. 
+ * Worker threads allow Node.js to execute JavaScript in parallel on separate threads.
+ * This can significantly improve performance for CPU-intensive operations 
+ * without blocking the main event loop. Each worker thread runs in its own V8 
+ * instance and has its own isolated memory.
+ * 
+ * For more information on Worker Threads, refer to the official Node.js documentation:
+ * https://nodejs.org/api/worker_threads.html
+ */
 
-const app = express();
+/**
+ * Concept of Message Queues:
+ * --------------------------
+ * Message queues provide an asynchronous communication mechanism for different 
+ * parts of a system to send messages between them. In the context of our app, 
+ * a message queue (implemented by Bull) allows us to offload intensive processing 
+ * tasks to a separate process or thread, ensuring the main application remains responsive.
+ * The main app adds tasks (or messages) to the queue, and worker processes or threads 
+ * pick up and process these tasks. This architecture efficiently distributes processing 
+ * load and decouples the main app from the processing logic.
+ * 
+ * For more information on Bull and its underlying concepts, visit:
+ * https://github.com/OptimalBits/bull
+ */
 
-// Use express-fileupload middleware to parse file uploads
+// Import necessary modules.
+const express = require('express'); // Express is a minimal and flexible Node.js web application framework.
+const fileUpload = require('express-fileupload'); // Middleware for handling `multipart/form-data` (file uploads).
+const { Worker } = require('worker_threads'); // Node.js native module for spawning worker threads.
+const Queue = require('bull'); // Job and message queue based on Redis.
+const os = require('os'); // Node.js native module to access OS-specific properties and methods.
+
+const app = express(); // Initialize an Express application.
+
+// Use express-fileupload middleware to handle file uploads. This allows files to be attached to the req object.
 app.use(fileUpload());
 
-// Initialize the processing queue
+// Initialize the job queue named 'file-processing' using Redis as the storage backend.
+// Here, the Redis instance is running locally on the default port 6379.
 const processingQueue = new Queue('file-processing', 'redis://127.0.0.1:6379');
 
-// Define the route for file upload and processing
+// Define the endpoint for file uploads. This is the main endpoint where files will be POSTed for processing.
 app.post('/upload', (req, res) => {
+    // Check if there are any uploaded files.
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    let sampleFile = req.files.sampleFile;
+    let sampleFile = req.files.sampleFile; // Access the uploaded file. You can choose a custom field name other than 'sampleFile'.
 
-    // Add file data to the processing queue
+    // Add the file data to our processing queue. This offloads the heavy processing from the main thread.
     processingQueue.add({ fileData: sampleFile.data });
 
+    // Send a response to the client indicating that the file is being processed.
     res.send('File uploaded and processing started!');
 });
 
-// Define the status endpoint to check the event loop
+// Define a simple status endpoint. This can be used to test if the server's main event loop is blocked.
 app.get('/status', (req, res) => {
     res.send('Event loop is not blocked');
 });
 
-// Listen to processing events and start a new worker thread for each task
+// Setup a processor for our queue tasks. This will execute the task using worker threads.
+// We aim to process jobs in parallel based on the number of CPUs available. 
 processingQueue.process(os.cpus().length, (job) => {
     return new Promise((resolve, reject) => {
+        // Initialize a worker thread and pass the file data to it for processing.
         const worker = new Worker('./worker.js', { workerData: job.data.fileData });
+
+        // Listen for messages from the worker thread. This can be the result or any other messages.
         worker.on('message', resolve);
+
+        // Handle any errors from the worker thread.
         worker.on('error', reject);
+
+        // Handle the exit event of the worker. If the exit wasn't graceful, reject the promise.
         worker.on('exit', (code) => {
             if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
         });
     });
 });
 
-// Start the server
+// Start the Express server on port 3000.
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
 
-module.exports = app; // For testing purposes
+// Export the Express app (useful for testing).
+module.exports = app; 
+
--- worker.js
+++ worker.js
@@ -1,7 +1,27 @@
+/**
+ * The worker_threads module provides a way to create multiple environments running
+ * on independent threads, and to create worker threads to perform CPU-intensive
+ * JavaScript operations. In this module, we use the workerData to receive input
+ * data and the parentPort to communicate the results back to the parent thread.
+ */
 const { parentPort, workerData } = require('worker_threads');
+
+/**
+ * The csv-parser is a Node.js package that provides a fast CSV parsing and stringifying 
+ * library for Node.js. It is stream-based, so it can handle large files without 
+ * loading the entire CSV into memory.
+ */
 const csv = require('csv-parser');
+
+/**
+ * The Readable stream, part of Node.js's Stream module, provides a way to read data 
+ * from a source. In this case, we're going to use it to read our CSV data.
+ */
 const { Readable } = require('stream');
 
+/**
+ * A simple function to check if a number is prime.
+ */
 function isPrime(num) {
     for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
         if (num % i === 0) return false;
@@ -9,14 +29,26 @@ function isPrime(num) {
     return num > 1;
 }
 
+/**
+ * Function to process each row of the CSV. It includes a simulated delay to 
+ * mimic some complex processing on each row.
+ */
 function processRow(row) {
-    for (let i = 0; i < 1e7; i++) {} // Simulated delay
+    // Simulated CPU-intensive task to introduce a delay
+    for (let i = 0; i < 1e7; i++) {} 
 
+    // Here we check if the 'someNumericField' from our CSV is a prime number.
+    // More complex processing logic can be added here if necessary.
     if (isPrime(row.someNumericField)) {
         // Add more complex processing here if necessary
     }
 }
 
+/**
+ * We create a readable stream from the CSV data passed to this worker thread.
+ * Then we pipe this stream through the csv-parser to process each row.
+ * After all rows are processed, we send a message back to the parent thread.
+ */
 const stream = Readable.from(workerData.toString());
 stream.pipe(csv())
     .on('data', (row) => {
